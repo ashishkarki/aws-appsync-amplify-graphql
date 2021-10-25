9fd689c529388686eed99de82a05b6f5fae9a831
Ashish Karki <ashishk@Ashishs-MacBook-Pro.local>
2021-10-25 16:54:31 +0545

    appsync-blogfinal project: added aws-amplify configuration to connect this react frontend to aws backend

--- appsync-blogfinal/src/index.js
+++ appsync-blogfinal/src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom'
 import './index.scss'
 import App from './App'
 
+import Amplify from 'aws-amplify'
+import aws_exports from './aws-exports'
+
+// configure aws amplify to work with this frontend app
+Amplify.configure(aws_exports)
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
