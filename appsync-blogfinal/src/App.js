db21a4e96d280d676198a93ed621e1a8871ef22c
Ashish Karki <ashishk@Ashishs-MacBook-Pro.local>
2021-11-01 18:21:13 +0545

    appsync-blogfinal project: figured out the fix for amplify dependencies and added auth screen

--- appsync-blogfinal/src/App.js
+++ appsync-blogfinal/src/App.js
@@ -2,6 +2,14 @@ import './App.scss'
 import CreatePost from './components/CreatePost/CreatePost'
 import DisplayPosts from './components/DisplayPosts/DisplayPosts'
 
+import Amplify, { Auth } from 'aws-amplify'
+import aws_exports from './aws-exports'
+import { withAuthenticator } from '@aws-amplify/ui-react'
+
+Amplify.configure(aws_exports)
+
+Auth.configure(aws_exports)
+
 function App() {
   return (
     <div className="App">
@@ -12,4 +20,4 @@ function App() {
   )
 }
 
-export default App
+export default withAuthenticator(App)
